2506dd16be6e451665356f5a5dfdcc583df822dc
dk808zelnode <47246006+dk808zelnode@users.noreply.github.com>
2019-02-25 08:04:38 -0800

    Update queue_pos.sh

--- queue_pos.sh
+++ queue_pos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ZNLISTCMD="zelcash-cli listzelnodes 2>/dev/null"
+ZNLISTCMD="zelcash-cli listzelnodes |jq -r '[.[] |{(.txhash):(.status+" "+(.version|tostring)+" "+.addr+" "+(.lastseen|tostring)+" "+(.activetime|tostring)+" "+(.lastpaid|tostring)+" "+.ipaddress)}]|add'
+ 2>/dev/null"
 
 ZNADDR=$1
 
